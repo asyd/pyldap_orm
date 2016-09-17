366a99cab0c662e36f2320e73c7befc713edf994
Bruno Bonfils <asyd@asyd.net>
2016-09-18 00:10:30 +0200

    Initial comit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyldap_orm',
-      version='0.2.3',
+      version='0.0.1',
       description='A simple LDAP ORM to make LDAP access using Python Objects',
       url='http://github.com/storborg/funniest',
       author='Bruno Bonfils',
