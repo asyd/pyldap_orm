a5ba43361b7171af509b02dc909990a950048f23
Bruno Bonfils <asyd@asyd.net>
2016-09-28 02:04:27 +0200

    Closes #2, #3.

--- pyldap_orm/core.py
+++ pyldap_orm/core.py
@@ -15,7 +15,7 @@ class LDAPSession(object):
     LDAPSession represents a connection to a LDAP server. For the moment, only plain LDAP is supported.
     STARTTLS and TLS will be supported in the a future release.
 
-    This class also manage initial binding if required. If no credentials are given, an anonymouns bind
+    This class also manage initial binding if required. If no credentials are given, an anonymous bind
     will be performed.
 
     Tested servers:
@@ -26,14 +26,18 @@ class LDAPSession(object):
     STARTTLS = 1
     LDAPS = 2
 
+    SASL_EXTERNAL = 1
+
     bind_dn = None
     credential = None
     backend = None
 
     def __init__(self, backend, bind_dn=None, credential=None, mode=PLAIN,
-                 cacertdir='/etc/ssl/certs',
+                 sasl=None,
                  cert=None,
-                 key=None):
+                 key=None,
+                 cacertdir='/etc/ssl/certs',
+                 ):
         """
         Create a LDAPSession by connecting to the LDAP server, and perform optional initial bind if bind_dn and
         credential are defined, otherwise perform an anonymous bind.
@@ -42,6 +46,9 @@ class LDAPSession(object):
         :param bind_dn:
         :param credential:
         :param mode: Transport mode, must be self.PLAIN (the default), self.STARTTLS or self.LDAPS
+        :param sasl: SASL Mechanism mode (only EXTERNAL is supported in this version)
+        :param cert: Client certificate (optional), must be in PEM format
+        :param key: Certificate private key, must be in PEM format with no password
         :param cacertdir: Directory of CA certificates, default is /etc/ssl/certs
         """
         self.backend = backend
@@ -51,34 +58,39 @@ class LDAPSession(object):
         logger.debug("LDAP _session created, id: {}".format(id(self)))
 
         # Switch to LDAPS mode if ldaps is backend start with 'ldaps'
-        if 'ldaps' == backend[0-5]:
+        if 'ldaps' == backend[0 - 5]:
             mode = self.LDAPS
 
-        # Set CACERTDIR if using encryption
-        if mode in (self.STARTTLS, self.LDAPS):
+        # Set CACERTDIR and REQUIRED_CERT to TLS_DEMAND (validation required) if needed
+        if mode in (self.STARTTLS, self.LDAPS) and cacertdir is not None:
             ldap.set_option(ldap.OPT_X_TLS_CACERTDIR, cacertdir)
             ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_DEMAND)
 
+        # Set client certificate is both cert and key are provided
         if cert is not None and key is not None:
             ldap.set_option(ldap.OPT_X_TLS_CERTFILE, cert)
             ldap.set_option(ldap.OPT_X_TLS_KEYFILE, key)
 
         try:
-            self._server = ldap.initialize(self.backend)
-        except ldap.SERVER_DOWN:
-            pass
+            self._server = ldap.initialize(self.backend, bytes_mode=False)
+        except ldap.LDAPError as e:
+            catch_ldap_exception(e)
 
+        # Proceed STARTTLS
         if mode == self.STARTTLS:
             try:
                 self._server.start_tls_s()
             except ldap.LDAPError as e:
                 catch_ldap_exception(e)
-            # Perform a SASL binding with EXTERNAL mechanism
-            if cert is not None and key is not None:
-                try:
-                    self._server.sasl_bind_s(None, 'EXTERNAL', None)
-                except ldap.LDAPError as e:
-                    catch_ldap_exception(e)
+
+        # Perform a SASL binding with EXTERNAL mechanism if cert and key are provided
+        if cert is not None and key is not None and sasl == self.SASL_EXTERNAL:
+            try:
+                # No need to set bind_dn and credential, the server will use the certificate to map a LDAP entry
+                self._server.sasl_bind_s(None, 'EXTERNAL', None)
+            except ldap.LDAPError as e:
+                catch_ldap_exception(e)
+        # Otherwise, use simple_bind_s
         else:
             if bind_dn is not None and credential is not None:
                 logger.debug("LDAP _session: bind as {}".format(bind_dn))
@@ -332,7 +344,6 @@ class LDAPModelList(object):
     A list of LDAPObject instances
     """
     children = None  # type: LDAPObject
-    filter = None
 
     def __init__(self, session=None):
         """
@@ -346,7 +357,6 @@ class LDAPModelList(object):
         self._objects = list()
         self._dn = None
         self._session = session
-        # TODO: check children
 
     def _parse_multiple(self, entries):
         for entry in entries:
