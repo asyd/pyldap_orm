213ec6d1a93b34a077a4f4389a5c45efa1320109
Bruno Bonfils <asyd@asyd.net>
2016-10-16 00:34:13 +0200

    Trying to fix RTD issue

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['ldap', 'pyasn1', 'ldap.modlist', 'ldap.controls']
+MOCK_MODULES = ['ldap', 'pyasn1', 'ldap.modlist', 'ldap.controls', 'pyasn1.type', 'pyasn1.codec']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 #
